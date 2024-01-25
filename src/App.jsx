62eb5ccbe0e5f8dd9ee62b62758e644a63b22711
mominul-01 <mominul.cse21@gmail.com>
2024-01-26 01:01:39 +0600

    Project Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Task Management Application....first-letter:
+      </h1>
+    </>
+  );
+}
+
+export default App;
