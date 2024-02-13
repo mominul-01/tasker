0525cfc8522e6934583f16a1ca6c2c4cb6037b7a
mominul-01 <mominul.cse21@gmail.com>
2024-02-14 01:48:31 +0600

    Task Board all feature done main feature Search Task, Edit task, Delete task, Delete All task

---
+++ src/components/task/NoTasksFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoTasksFound = () => {
+  return (
+    <div>
+      <p className="text-center text-3xl">No Tasks Found. Please Add One.</p>
+    </div>
+  );
+};
+
+export default NoTasksFound;
